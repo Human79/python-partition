da2d8a90201fb277d88fb80449ce5c872797222d
A Human <84245065+Human79@users.noreply.github.com>
2023-06-21 17:45:50 +0530

    Add ability to Benchmark drives on Windows

--- python_partiton.py
+++ python_partiton.py
@@ -1,6 +1,29 @@
-print("Getting partition data")
+import os
+import pyuac
 import psutil
 
-for disk in psutil.disk_partitions():
+x=int(input("Type (1) to get parition data or (2) to benchmark the disk"))
+
+if x== 1:
+   print("Getting partition data")
+   for disk in psutil.disk_partitions():
     if disk.fstype:
         print(disk.device, psutil.disk_usage(disk.mountpoint))
+
+def main():
+  print("Running Benchmarks")
+  os.system('cmd /k "winsat disk -drive c' )
+    # The window will disappear as soon as the program exits!
+input("Press enter to continue. >")
+
+
+
+if __name__ == "__main__":
+    if not pyuac.isUserAdmin():
+        print("Re-launching as admin!")
+        pyuac.runAsAdmin()
+    else:        
+        main()  # Already an admin here.
+elif x== 2:
+   main()
+
